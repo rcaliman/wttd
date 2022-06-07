c4f0aa6cb124827ad1f48c39945eca5f6af47b69
Rodrigo Caliman <rcaliman@gmail.com>
2022-06-07 20:02:16 -0300

    correcao do static

--- eventex/settings.py
+++ eventex/settings.py
@@ -74,6 +74,7 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 default_dburl = 'sqlite:///' + str(BASE_DIR / 'db.sqlite3')
+
 DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
@@ -114,7 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = str(BASE_DIR) + '/' + 'staticfiles'
+print(STATIC_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
